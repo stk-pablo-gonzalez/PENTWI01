4de18dae499550f3e14caf238b83b5bac1780a56
Pablo Gonzalez <pablo@Pablos-Mac-mini.local>
2017-12-07 01:10:44 -0600

    Results

--- src/calculator.js
+++ src/calculator.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
     var result = 0;
     var temp = 0;
+    var currentOperation = '';
 
     $("button").click(function() {
         var data = $(this).attr("data-value");
@@ -9,15 +10,48 @@ $(document).ready(function() {
         if(data == "clear") {
             temp = 0;
             result = 0;
+        } else if(data == "multiply") {
+            if(result == 0)
+                result
         } else {
-            var value = $("#txtResult").val();
-            if(value == "0") value = "";
-            value += "" + data;
-            setResult(value);
+            
         }
     });
 
+    function displayResult() {
+        $("#txtResult").val(result);
+    }
+
     function setResult(value) {
-        $("#txtResult").val(value);
+        $("#txtResult").val();
+    }
+
+    function getResult() {
+        return $("#txtResult").val();
+    }
+
+    function appendDigitToResult() {
+        var value = $("#txtResult").val();
+        if(value == "0") value = "";
+        value += "" + data;
+        setResult(value);
+    }
+
+    var operations = {
+        sum: function(a, b) {
+            return a + b;
+        },
+        substract: function(a, b) {
+            return a - b;
+        },
+        multiply: function(a, b) {
+            return a * b;
+        },
+        divide: function(a, b) {
+            return a / b;
+        },
+        pct: function(a, b) {
+            return a * (b / 100);
+        }
     }
 });
